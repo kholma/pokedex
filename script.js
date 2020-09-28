4863accaef6c1ccd95db1da6f65901e5a5f17a64
Kylie Holman <kholma@uwo.ca>
2020-09-27 23:59:27 -0400

    added styling for dynamic list images

--- script.js
+++ script.js
@@ -10,106 +10,184 @@ unorderedList.setAttribute("id","searchUL");
 
 var li1=document.createElement("LI");
 unorderedList.appendChild(li1);
-var p1=document.createTextNode("Bulbasaur- Number: 1");
+var p1=document.createTextNode("Bulbasaur- Number: 1, Rarity: 4, Evolution: 1, Charge Move: Sludge Bomb");
 li1.appendChild(p1);
-var p1num=document.createTextNode("Number: 1");
-
+var img1=document.createElement("IMG");
+img1.src="pokemon/1.png";
+img1.width="50";
+li1.appendChild(img1);
 
 var li2=document.createElement("LI");
-var p2=document.createTextNode("Ivysaur- Number: 2");
+var p2=document.createTextNode("Ivysaur- Number: 2, Rarity: 5, Evolution: 2, Charge Move: Sludge Bomb");
 unorderedList.appendChild(li2);
 li2.appendChild(p2);
+var img2=document.createElement("IMG");
+img2.src="pokemon/2.png";
+img2.width="50";
+li2.appendChild(img2);
 
 var li3=document.createElement("LI");
-var p3=document.createTextNode("Venusaur- Number:3");
+var p3=document.createTextNode("Venusaur- Number: 3, Rarity: 6, Evolution:3, Charge Move: Sludge bomb");
 unorderedList.appendChild(li3);
 li3.appendChild(p3);
+var img3=document.createElement("IMG");
+img3.src="pokemon/3.png";
+img3.width="50";
+li3.appendChild(img3);
 
 var li4=document.createElement("LI");
-var p4=document.createTextNode("Charmander- Number:4");
+var p4=document.createTextNode("Charmander- Number: 4, Rarity: 4, Evolution: 1, Charge Move: Flame Burst");
 unorderedList.appendChild(li4);
 li4.appendChild(p4);
+var img4=document.createElement("IMG");
+img4.src="pokemon/4.png";
+img4.width="50";
+li4.appendChild(img4);
 
 var li5=document.createElement("LI");
-var p5=document.createTextNode("Charmeleon- Number:5");
+var p5=document.createTextNode("Charmeleon- Number: 5, Rarity: 5, Evolution: 2, Charge Move: Flame Burst");
 unorderedList.appendChild(li5);
 li5.appendChild(p5);
+var img5=document.createElement("IMG");
+img5.src="pokemon/5.png";
+img5.width="50";
+li5.appendChild(img5);
 
 var li6=document.createElement("LI");
-var p6=document.createTextNode("Charizard- Number:6");
+var p6=document.createTextNode("Charizard- Number: 6, Rarity: 6, Evolution: 3, Charge Move: Fire Blast");
 unorderedList.appendChild(li6);
 li6.appendChild(p6);
+var img6=document.createElement("IMG");
+img6.src="pokemon/6.png";
+img6.width="50";
+li6.appendChild(img6);
 
 var li7=document.createElement("LI");
-var p7=document.createTextNode("Squirtle- Number:7");
+var p7=document.createTextNode("Squirtle- Number: 7, Rarity: 4, Evolution: 1, Charge Move: Aqua Jet");
 unorderedList.appendChild(li7);
 li7.appendChild(p7);
+var img7=document.createElement("IMG");
+img7.src="pokemon/7.png";
+img7.width="50";
+li7.appendChild(img7);
 
 var li8=document.createElement("LI");
-var p8=document.createTextNode("Wartortle- Number:8");
+var p8=document.createTextNode("Wartortle- Number: 8, Rarity: 5, Evolution: 2, Charge Move: Aqua Jet");
 unorderedList.appendChild(li8);
 li8.appendChild(p8);
+var img8=document.createElement("IMG");
+img8.src="pokemon/8.png";
+img8.width="50";
+li8.appendChild(img8);
 
 var li9=document.createElement("LI");
-var p9=document.createTextNode("Blastoise- Number:9");
+var p9=document.createTextNode("Blastoise- Number: 9, Rarity: 6, Evolution: 3, Charge Move: Icea Beam");
 unorderedList.appendChild(li9);
 li9.appendChild(p9);
+var img9=document.createElement("IMG");
+img9.src="pokemon/9.png";
+img9.width="50";
+li9.appendChild(img9);
 
 var li10=document.createElement("LI");
-var p10=document.createTextNode("Caterpie- Number:10");
+var p10=document.createTextNode("Caterpie- Number: 10, Rarity: 1, Evolution: 1, Charge Move: Struggle");
 unorderedList.appendChild(li10);
 li10.appendChild(p10);
+var img10=document.createElement("IMG");
+img10.src="pokemon/10.png";
+img10.width="50";
+li10.appendChild(img10);
 
 var li11=document.createElement("LI");
-var p11=document.createTextNode("Metapod- Number:11");
+var p11=document.createTextNode("Metapod- Number: 11, Rarity: 3, Evolution: 2, Charge Move: Struggle");
 unorderedList.appendChild(li11);
 li11.appendChild(p11);
+var img11=document.createElement("IMG");
+img11.src="pokemon/11.png";
+img11.width="50";
+li11.appendChild(img11);
 
 var li12=document.createElement("LI");
-var p12=document.createTextNode("Butterfree- Number:12");
+var p12=document.createTextNode("Butterfree- Number: 12, Rarity: 4, Evolution: 3, Charge Move: Psychic");
 unorderedList.appendChild(li12);
 li12.appendChild(p12);
+var img12=document.createElement("IMG");
+img12.src="pokemon/12.png";
+img12.width="50";
+li12.appendChild(img12);
 
 var li13=document.createElement("LI");
-var p13=document.createTextNode("Weedle- Number:13");
+var p13=document.createTextNode("Weedle- Number: 13, Rarity: 1, Evolution: 1, Charge Move: Struggle");
 unorderedList.appendChild(li13);
 li13.appendChild(p13);
+var img13=document.createElement("IMG");
+img13.src="pokemon/13.png";
+img13.width="50";
+li13.appendChild(img13);
 
 var li14=document.createElement("LI");
-var p14=document.createTextNode("Kakuna- Number:14");
+var p14=document.createTextNode("Kakuna- Number: 14, Rarity: 3, Evolution: 2, Charge Move: Struggle");
 unorderedList.appendChild(li14);
 li14.appendChild(p14);
+var img14=document.createElement("IMG");
+img14.src="pokemon/14.png";
+img14.width="50";
+li14.appendChild(img14);
 
 var li15=document.createElement("LI");
-var p15=document.createTextNode("Beedrill- Number:15");
+var p15=document.createTextNode("Beedrill- Number: 15, Rarity: 4, Evolution: 3, Charge Move: Aerial Ace");
 unorderedList.appendChild(li15);
 li15.appendChild(p15);
+var img15=document.createElement("IMG");
+img15.src="pokemon/15.png";
+img15.width="50";
+li15.appendChild(img15);
 
 var li16=document.createElement("LI");
-var p16=document.createTextNode("Pidgey- Number:16");
+var p16=document.createTextNode("Pidgey- Number: 16, Rarity: 1, Evolution: 1, Charge Move: Twister");
 unorderedList.appendChild(li16);
 li16.appendChild(p16);
+var img16=document.createElement("IMG");
+img16.src="pokemon/16.png";
+img16.width="50";
+li16.appendChild(img16);
 
 var li17=document.createElement("LI");
-var p17=document.createTextNode("Pidgeotto- Number:17");
+var p17=document.createTextNode("Pidgeotto- Number: 17, Rarity: 3, Evolution: 2, Charge Move: Twister");
 unorderedList.appendChild(li17);
 li17.appendChild(p17);
+var img17=document.createElement("IMG");
+img17.src="pokemon/17.png";
+img17.width="50";
+li17.appendChild(img17);
 
 var li18=document.createElement("LI");
-var p18=document.createTextNode("Pidgeot- Number:18");
+var p18=document.createTextNode("Pidgeot- Number: 18, Rarity: 4, Evolution: 3, Charge Move: Hurricane");
 unorderedList.appendChild(li18);
 li18.appendChild(p18);
+var img18=document.createElement("IMG");
+img18.src="pokemon/18.png";
+img18.width="50";
+li18.appendChild(img18);
 
 var li19=document.createElement("LI");
-var p19=document.createTextNode("Rattata- Number:19");
+var p19=document.createTextNode("Rattata- Number: 19, Rarity: 1, Evolution: 1, Charge Move: Dig");
 unorderedList.appendChild(li19);
 li19.appendChild(p19);
+var img19=document.createElement("IMG");
+img19.src="pokemon/19.png";
+img19.width="50";
+li19.appendChild(img19);
 
 
 var li20=document.createElement("LI");
-var p20=document.createTextNode("Raticate- Number:20");
+var p20=document.createTextNode("Raticate- Number: 20, Rarity: 4, Evolution: 2, Charge move: Dig");
 unorderedList.appendChild(li20);
 li20.appendChild(p20);
+var img20=document.createElement("IMG");
+img20.src="pokemon/20.png";
+img20.width="50";
+li20.appendChild(img20);
 
 
 function filterNameList(){
